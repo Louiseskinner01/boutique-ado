de4ae73112cd39eed78a7680473cb31a555d5db6
Louise sKinner <louise.skinner40@gmail.com>
2026-03-24 12:58:35 +0000

    Set up allauth

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import HttpResponse
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+     path('', lambda request: HttpResponse("Home page")),  # test page
+    
+
 ]
